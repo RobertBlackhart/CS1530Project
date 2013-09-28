989371468852b823f57b8f72d1bc6fadd0ba6697
Robert McDermot <robert.mcdermot@gmail.com>
2013-09-28 17:12:38 -0400

    attempt to fix appengine not compiling after merge with Rickie's code.

--- ClassWeb/src/com/cs1530/group4/addendum/client/AdminAddCourse.java
+++ ClassWeb/src/com/cs1530/group4/addendum/client/AdminAddCourse.java
@@ -6,11 +6,12 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IntegerBox;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.HorizontalSplitPanel;
 
 public class AdminAddCourse extends Composite
 {
@@ -28,85 +29,91 @@ public class AdminAddCourse extends Composite
 
 		FlexTable flexTable = new FlexTable();
 		verticalPanel.add(flexTable);
-		flexTable.setSize("100%", "50%");
+		verticalPanel.setCellHeight(flexTable, "100%");
+		flexTable.setSize("526px", "244px");
 
 		Label lblSubjectCode = new Label("Subject Code");
-		flexTable.setWidget(0, 0, lblSubjectCode);
-		flexTable.getCellFormatter().setStyleName(0, 0, "wahtever");
+		lblSubjectCode.setStyleName("whatever");
+		flexTable.setWidget(0, 1, lblSubjectCode);
+		lblSubjectCode.setWidth("146px");
 
 		subjectTextBox = new TextBox();
-		flexTable.setWidget(0, 1, subjectTextBox);
-		subjectTextBox.setWidth("100%");
+		flexTable.setWidget(0, 3, subjectTextBox);
+		subjectTextBox.setWidth("90%");
 
 		Label lblNumber = new Label("Number");
-		flexTable.setWidget(1, 0, lblNumber);
-		
+		lblNumber.setStyleName("whatever");
+		flexTable.setWidget(1, 1, lblNumber);
+
 		numberIntegerBox = new IntegerBox();
-		flexTable.setWidget(1, 1, numberIntegerBox);
-		numberIntegerBox.setWidth("100%");
+		flexTable.setWidget(1, 3, numberIntegerBox);
+		numberIntegerBox.setWidth("90%");
 
 		Label lblName = new Label("Name");
-		flexTable.setWidget(2, 0, lblName);
+		lblName.setStyleName("whatever");
+		flexTable.setWidget(2, 1, lblName);
 
 		nameTextBox = new TextBox();
-		flexTable.setWidget(2, 1, nameTextBox);
-		nameTextBox.setWidth("100%");
+		flexTable.setWidget(2, 3, nameTextBox);
+		nameTextBox.setWidth("90%");
 
 		Label lblDescription = new Label("Description");
-		flexTable.setWidget(3, 0, lblDescription);
+		lblDescription.setStyleName("whatever");
+		flexTable.setWidget(3, 1, lblDescription);
+
+		Image image = new Image((String) null);
+		flexTable.setWidget(3, 2, image);
 
 		descriptionTextBox = new TextBox();
-		flexTable.setWidget(3, 1, descriptionTextBox);
-		descriptionTextBox.setWidth("100%");
-		
-		HorizontalSplitPanel horizontalSplitPanel = new HorizontalSplitPanel();
-		verticalPanel.add(horizontalSplitPanel);
-		horizontalSplitPanel.setHeight("37px");
-		
-				Button btnNewButton = new Button("Add Course");
-				horizontalSplitPanel.setLeftWidget(btnNewButton);
-				btnNewButton.setSize("100%", "100%");
-				
-						Button btnBackToSearch = new Button("Back to Search");
-						horizontalSplitPanel.setRightWidget(btnBackToSearch);
-						btnBackToSearch.setSize("100%", "100%");
-						btnBackToSearch.addClickHandler(new ClickHandler()
-						{
-							public void onClick(ClickEvent event)
-							{
-								main.setContent(new ClassSearch(main),"classSearch");
-							}
-						});
-				btnNewButton.addClickHandler(new ClickHandler()
+		flexTable.setWidget(3, 3, descriptionTextBox);
+		descriptionTextBox.setWidth("90%");
+
+		Button btnNewButton = new Button("Add Course");
+		flexTable.setWidget(4, 1, btnNewButton);
+		btnNewButton.setWidth("90%");
+		btnNewButton.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent event)
+			{
+				String code = subjectTextBox.getText();
+				int num = numberIntegerBox.getValue();
+				String name = nameTextBox.getText();
+				String desc = descriptionTextBox.getText();
+				UserServiceAsync loginService = UserService.Util.getInstance();
+				// Set up the callback object.
+				AsyncCallback<Void> callback = new AsyncCallback<Void>()
 				{
-					public void onClick(ClickEvent event)
+					@Override
+					public void onFailure(Throwable caught)
 					{
-						String code = subjectTextBox.getText();
-						int num = numberIntegerBox.getValue();
-						String name = nameTextBox.getText();
-						String desc = descriptionTextBox.getText();
-						UserServiceAsync loginService = UserService.Util.getInstance();
-						// Set up the callback object.
-						AsyncCallback<Void> callback = new AsyncCallback<Void>()
-						{
-							@Override
-							public void onFailure(Throwable caught)
-							{
-							}
-
-							@Override
-							public void onSuccess(Void v)
-							{
-								subjectTextBox.setText("");
-								numberIntegerBox.setText("");
-								nameTextBox.setText("");
-								descriptionTextBox.setText("");
-							}
-						};
-
-						loginService.adminAddCourse(code, num, name, desc, callback);
 					}
-				});
+
+					@Override
+					public void onSuccess(Void v)
+					{
+						subjectTextBox.setText("");
+						numberIntegerBox.setText("");
+						nameTextBox.setText("");
+						descriptionTextBox.setText("");
+					}
+				};
+
+				loginService.adminAddCourse(code, num, name, desc, callback);
+			}
+		});
+
+		Button btnBackToSearch = new Button("Back to Search");
+		flexTable.setWidget(4, 3, btnBackToSearch);
+		btnBackToSearch.setWidth("95%");
+		flexTable.getCellFormatter().setHorizontalAlignment(4, 1, HasHorizontalAlignment.ALIGN_LEFT);
+		flexTable.getCellFormatter().setHorizontalAlignment(4, 3, HasHorizontalAlignment.ALIGN_LEFT);
+		btnBackToSearch.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent event)
+			{
+				main.setContent(new ClassSearch(main), "classSearch");
+			}
+		});
 	}
 
 }
