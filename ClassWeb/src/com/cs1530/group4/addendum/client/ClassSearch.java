47b0380d208da74e62ddab4090759bd4f651cdd8
Robert McDermot <robert.mcdermot@gmail.com>
2013-09-30 13:25:48 -0400

    fix merge errors

--- ClassWeb/src/com/cs1530/group4/addendum/client/ClassSearch.java
+++ ClassWeb/src/com/cs1530/group4/addendum/client/ClassSearch.java
@@ -138,69 +138,65 @@ public class ClassSearch extends Composite
 		verticalPanel.add(resultsBox);
 		resultsBox.setVisibleItemCount(5);
 		resultsBox.setWidth("500px");
-		
-				Button btnAddSelectedClasses = new Button("Add Selected Classes");
-				btnAddSelectedClasses.setStyleName("ADCButton");
-				btnAddSelectedClasses.addClickHandler(new ClickHandler()
-				{
-					public void onClick(ClickEvent event)
-					{
-						ArrayList<String> courses = new ArrayList<String>();
-						for(int i = 0; i < resultsBox.getItemCount(); i++)
-						{
-							if(resultsBox.isItemSelected(i))
-							{
-								String item = resultsBox.getValue(i);
-								String course = item.substring(0, item.indexOf(' '));
-								courses.add(course);
-							}
-						}
-						AsyncCallback<Void> callback = new AsyncCallback<Void>()
-						{
-							@Override
-							public void onFailure(Throwable caught)
-							{
-							}
 
-							@Override
-							public void onSuccess(Void v)
-							{
-								String user = Cookies.getCookie("loggedIn");
-								main.setContent(new Profile(main,user),"profile-"+user);
-							}
-						};
-						
-						if(courses.size() > 0)
-							userService.userAddCourse(Cookies.getCookie("loggedIn"), courses, callback);
-						else
-						{
-							String user = Cookies.getCookie("loggedIn");
-							main.setContent(new Profile(main,user),"profile-"+user);
-						}
+		Button btnAddSelectedClasses = new Button("Add Selected Classes");
+		btnAddSelectedClasses.setStyleName("ADCButton");
+		btnAddSelectedClasses.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent event)
+			{
+				ArrayList<String> courses = new ArrayList<String>();
+				for(int i = 0; i < resultsBox.getItemCount(); i++)
+				{
+					if(resultsBox.isItemSelected(i))
+					{
+						String item = resultsBox.getValue(i);
+						String course = item.substring(0, item.indexOf(' '));
+						courses.add(course);
 					}
-				});
-				verticalPanel.add(btnAddSelectedClasses);
-		
+				}
+				AsyncCallback<Void> callback = new AsyncCallback<Void>()
+				{
+					@Override
+					public void onFailure(Throwable caught)
+					{
+					}
+
+					@Override
+					public void onSuccess(Void v)
+					{
+						String user = Cookies.getCookie("loggedIn");
+						main.setContent(new Profile(main, user), "profile-" + user);
+					}
+				};
+
+				if(courses.size() > 0)
+					userService.userAddCourse(Cookies.getCookie("loggedIn"), courses, callback);
+				else
+				{
+					String user = Cookies.getCookie("loggedIn");
+					main.setContent(new Profile(main, user), "profile-" + user);
+				}
+			}
+		});
+		verticalPanel.add(btnAddSelectedClasses);
+
 		FlexTable flexTable_1 = new FlexTable();
 		verticalPanel.add(flexTable_1);
 		verticalPanel.setCellVerticalAlignment(flexTable_1, HasVerticalAlignment.ALIGN_BOTTOM);
-		
+
 		Label lblNewLabel = new Label("Couldn't find your course? Why not try ");
 		flexTable_1.setWidget(0, 0, lblNewLabel);
-		
-				Anchor addNewCourse = new Anchor("Add New Course");
-				addNewCourse.setHTML("Adding a New Course");
-				flexTable_1.setWidget(0, 1, addNewCourse);
-				addNewCourse.addClickHandler(new ClickHandler()
-				{
-					public void onClick(ClickEvent event)
-					{
-<<<<<<< HEAD
-						main.setContent(new AddNewCourse(main),"addNewCourse");
-=======
-						main.setContent(new AdminAddCourse(main),"adminAddCourse");
->>>>>>> refs/remotes/origin/master
-					}
-				});
+
+		Anchor addNewCourse = new Anchor("Add New Course");
+		addNewCourse.setHTML("Adding a New Course");
+		flexTable_1.setWidget(0, 1, addNewCourse);
+		addNewCourse.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent event)
+			{
+				main.setContent(new AddNewCourse(main), "addNewCourse");
+			}
+		});
 	}
 }
